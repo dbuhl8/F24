b9f5d32f80e27a1125c6cae7d1f511a1d86bf360
Dante <dbuhl@ucsc.edu>
2024-12-10 01:28:22 -0800

    Did some fortran for sim time est

---
+++ DFD/scaling_analysis/sim_time_est.f90
@@ -0,0 +1,11 @@
+!File: sim_time_est.f90
+!Author: Dante Buhl
+!Purpose: to estimate the number of timesteps needed to obtain simulations in a
+!statistically stationary state. 
+
+program sim_time_est
+
+  implicit none
+
+
+end program sim_time_est
