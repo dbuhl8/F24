32368c918912f8669a1e0aacf14980b9a4631dd4
Dante Buhl <dbuhl@ucsc.edu>
2024-12-14 18:22:43 -0800

    Added some more averages and updated the fortran file to average them

--- DFD/scaling_analysis/PADDIM_data/avg_files.f90
+++ DFD/scaling_analysis/PADDIM_data/avg_files.f90
@@ -2,18 +2,26 @@ program avg_files
 
   implicit none
 
-  integer, parameter :: num_files=13, num_lines=95
+  integer, parameter :: num_files=17, num_lines=95
   integer :: i, j
   real :: time_per_step
   real, dimension(num_files):: avg_tpt
-  character(len=12), dimension(num_files) :: files
+  character(len=20), dimension(num_files) :: files, conf_text
   integer, dimension(num_files) :: np
   
-  files = [character(len=12) :: "lr64.dat", &
-    "lr128.dat", "lr256.dat", "lr512.dat", "lr1024.dat", &
-    "hr256.dat", "hr512.dat", "hr1024.dat", "hr2048.dat",&
-    "llr32.dat", "llr64.dat", "llr128.dat", "llr256.dat"]
-  np = [64, 128, 256, 512, 1024, 256, 512, 1024, 2048, 32, 64, 128, 256]
+  files = [character(len=20) :: "lr64.txt", &
+    "lr128.txt", "lr256.txt", "lr256_16x16.txt", "lr512.txt", "lr1024.txt", &
+    "hr256.txt", "hr512.txt", "hr1024.txt", "hr1024_16x64.txt","hr2048.txt",&
+    "hr2048_16x128.txt", "hr2048_8x256.txt",&
+    "llr32.txt", "llr64.txt", "llr128.txt", "llr256.txt"]
+  np = [64, 128, 256, 256, 512, 1024,&
+         256, 512, 1024, 1024, 2048, 2048, 2048,&
+         32, 64, 128, 256]
+  conf_text = [character(len=20) :: "8x8", &
+    "8x16", "8x32","16x16", "16x32", "32x32", &
+    "8x32", "16x32", "32x32", "16x64","32x64",&
+    "16x128", "8x256",&
+    "4x8", "8x8", "8x16", "16x16"]
 
   time_per_step = 0.
   avg_tpt = 0.
@@ -29,18 +37,22 @@ program avg_files
   avg_tpt = avg_tpt/num_lines
 
   open(30, file="scaling.dat")
-  do i = 1, 5
-    write(30, "(I4,F8.3)")  np(i), avg_tpt(i)
+  write(30,*) "# Resolution: 256x256x64"
+  write(30,*) "# np   tpt    config"
+  do i = 1, 6
+    write(30, "(I4,F8.3,'   ',A)")  np(i), avg_tpt(i), conf_text(i)
   end do 
   write(30,*) 
-  write(30,*) 
-  do i = 6, 9
-    write(30, "(I4,F8.3)")  np(i), avg_tpt(i)
+  write(30,*) "# Resolution: 512x512x128"
+  write(30,*) "# np   tpt    config"
+  do i = 7, 13
+    write(30, "(I4,F8.3,'   ',A)")  np(i), avg_tpt(i), conf_text(i)
   end do 
   write(30,*) 
-  write(30,*) 
-  do i = 10, num_files
-    write(30, "(I4,F8.3)")  np(i), avg_tpt(i)
+  write(30,*) "# Resolution: 128x128x32"
+  write(30,*) "# np   tpt    config"
+  do i = 14, num_files
+    write(30, "(I4,F8.3,'   ',A)")  np(i), avg_tpt(i), conf_text(i)
   end do 
   close(30)   
 
